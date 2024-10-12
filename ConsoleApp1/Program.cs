1914c98dacd7df42d41dbaa1a85e05f9f30567be
Veniamin <benjaminshapkin@gmail.com>
2024-10-12 15:21:51 +0300

    create new app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿//This is comment to my code
+
+//rakendus küsib kasutajal sisestada oma nime
+//rakendus tervitab kasutajat nimepidi
+
+
+Console.WriteLine("Kirjuta oma nime");
+//string - sõne - строка
+string userName;
+
+userName = Console.ReadLine ();
+
+Console.WriteLine("Tere, " + userName + "!");
+Console.WriteLine($"Tere, {userName}!");
